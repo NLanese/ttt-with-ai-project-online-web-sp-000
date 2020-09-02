293cb75587666ec58ba1f006c80a056ad77b6246
Nick Lanese <nlanese21@gmail.com>
2020-09-02 18:42:51 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,18 +1,27 @@
 module Players
   class Computer < Player
 
-    def move(inputBoard)
-      passing = false
-      while !(passing)
-        randInput = gets.strip
-        if (inputBoard.valid_move?(ui))
-          passing = true
-        end
-      end
-      #.pry
-      return ui
-    end
+    WIN_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
 
+    def move(inputBoard)
+      if self != "X"
+        opp = "O"
+      else
+        opp = "X"
+      end
+      WIN_COMBINATIONS.each do | selectedCombo |
+        if (selectedCombo[0] == opp || selectedCombo[1] == opp)
+          return selectedCombo[2]
+    end
 
   end
 end
